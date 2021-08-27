4056c2fd4d894fc7f68b64d8530b621d35d9dbc9
yasu2704 <yasu2704.kuro@gmail.com>
2021-08-27 15:47:02 +0000

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import DotEnv from 'dotenv'
+import { Client, Intents } from 'discord.js'
+
+DotEnv.config()
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.once('ready', () => {
+    console.log('Ready!')
+})
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return
+
+    const {commandName} = interaction
+
+    switch (commandName) {
+        case 'ping':
+            await interaction.reply('Pong!')
+            break
+        case 'server':
+            await interaction.reply(`Server name: ${interaction.guild?.name}\nTotal members: ${interaction.guild?.memberCount}`)
+            break
+        case 'user':
+            await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`)
+            break
+    }
+})
+
+client.login(process.env.TOKEN)
+
