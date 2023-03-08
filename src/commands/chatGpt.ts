47e47ce49215a94d92a04687a9560ce28715555d
yasu2704 <yasu2704.kuro@gmail.com>
2023-03-08 16:02:14 +0000

    use gpt-3.5-turbo

--- src/commands/chatGpt.ts
+++ src/commands/chatGpt.ts
@@ -5,9 +5,15 @@ import {
   EmbedBuilder,
   SlashCommandBuilder,
 } from 'discord.js'
+import { Configuration, OpenAIApi } from 'openai'
 import DotEnv from 'dotenv'
 
 DotEnv.config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
 
 export default {
   data: new SlashCommandBuilder()
@@ -22,22 +28,33 @@ export default {
   async execute(
     interaction: ChatInputCommandInteraction<CacheType>
   ): Promise<void> {
+    const message = interaction.options.getString('message')
     const embed = new EmbedBuilder()
       .setDescription(`<@${interaction.user.id}> が会話を開始しました。`)
       .setColor(Colors.Green)
       .addFields({
         name: `${interaction.user.username}`,
-        value: `${interaction.options.getString('message')}`,
+        value: `${message}`,
       })
     await interaction.reply({ embeds: [embed] })
     const response = await interaction.fetchReply()
-    response.startThread({
-      name: `${interaction.user.username} - ${interaction.options
-        .getString('message')
-        ?.substring(0, 30)}`,
+    const thread = await response.startThread({
+      name: `${interaction.user.username} - ${message?.substring(0, 30)}`,
       reason: 'gpt-bot',
       autoArchiveDuration: 60,
       rateLimitPerUser: 1,
+    })
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: `${interaction.options.getString('message')}`,
+        },
+      ],
+    })
+    await thread.send({
+      content: `${completion.data.choices[0].message?.content}`,
     })
   },
 }
